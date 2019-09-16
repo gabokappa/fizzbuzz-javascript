7c9dc813a9f1dce06a0885bc73ddd4166764c19e
Student <student@Makerss-MacBook-Pro-7.local>
2019-09-16 15:58:59 +0100

    pass test divisible by 3 test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,14 +1,24 @@
 describe('FizzBuzz', function() {
   var fizzbuzz;
 
+  // beforeEach(funcion() {
+  //   fizzbuzz = new FizzBuzz();
+  // });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
       fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
+
   });
 
-  
+  describe('knows when a number is not', function() {
+    it('divisible by 3', function () {
+      fizzbuzz = new FizzBuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
 
 });
